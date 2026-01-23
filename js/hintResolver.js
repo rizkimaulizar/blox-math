52e2703611491e8f14d90d62cd357613149e951a
rizkimaulizar <129087037+rizkimaulizar@users.noreply.github.com>
2026-01-23 11:44:48 +0700

    Update hintResolver.js

--- js/hintResolver.js
+++ js/hintResolver.js
@@ -1,1 +1,16 @@
+// js/hintResolver.js
+import { NoobHintPanel } from '../hint-system/noob/NoobHintPanel.js';
+import { ProHintPanel } from '../hint-system/pro/ProHintPanel.js';
+
+export function getHintUI(levelKey) {
+  if (levelKey === 'NOOB') {
+    return NoobHintPanel;
+  }
+
+  if (levelKey === 'PRO') {
+    return ProHintPanel;
+  }
+
+  return null;
+}
 
