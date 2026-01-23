8dae95c6e59302cb1fc6cf08836fb862e1f71487
rizkimaulizar <129087037+rizkimaulizar@users.noreply.github.com>
2026-01-23 23:24:18 +0700

    Update levelConfig.js

--- js/core/levelConfig.js
+++ js/core/levelConfig.js
@@ -1,15 +1,23 @@
 export const GAME_LEVELS = {
   NOOB: {
+    key: 'NOOB',                // ✅ PENTING
     id: 'noob_builder',
     name: 'Foundation Field',
+
     allowedOps: ['ADD', 'MULTIPLY'],
     numberRange: { min: 1, max: 10 },
+
     progression: {
       warmup: 5,
       challenge: 4,
       finisher: 1
     },
-    assets: ['wood_block', 'stone_brick', 'grass_block', 'gold_block']
+
+    assets: [
+      'wood_block',
+      'stone_brick',
+      'grass_block',
+      'gold_block'
+    ]
   }
 };
-
