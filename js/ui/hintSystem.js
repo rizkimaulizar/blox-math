5272b601063c0fcc6516736db21187a009ea09c9
rizkimaulizar <129087037+rizkimaulizar@users.noreply.github.com>
2026-01-23 23:12:49 +0700

    Update hintSystem.js

--- js/ui/hintSystem.js
+++ js/ui/hintSystem.js
@@ -1,11 +1,18 @@
 import { NoobHintPanel } from './NoobHintPanel.js';
 import { ProHintPanel } from './ProHintPanel.js';
 
-export function showHint(level, text) {
-  if (level === 'NOOB') {
-    NoobHintPanel.show(text);
-  } else {
-    ProHintPanel.show();
-  }
-}
+export const HintSystem = {
+  show(level, text) {
+    if (level === 'NOOB') {
+      NoobHintPanel.show(text);
+    } else {
+      ProHintPanel.show();
+    }
+  },
 
+  hide(level) {
+    if (level === 'NOOB') {
+      NoobHintPanel.hide();
+    }
+  }
+};
